06a70d7a41f6ea598188ab95ed7647332814bf03
Timmy Jose <zoltan.jose@gmail.com>
2024-09-25 22:29:14 +0530

    feat: listing-2_1

--- listing-2_1/src/main.rs
+++ listing-2_1/src/main.rs
@@ -1,3 +1,36 @@
+macro_rules! my_vec {
+    () => {{
+        Vec::new()
+    }};
+
+    (make an empty vec) => {{
+        Vec::new()
+    }};
+
+    // not tsrictly needed
+    ($x:expr) => {{
+        vec![$x]
+    }};
+
+    ($($x:expr),+) => {{
+        let mut v = Vec::new();
+        $(
+            v.push($x);
+        )+
+            v
+    }}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let empty: Vec<i32> = my_vec![];
+    println!("{empty:#?}");
+
+    let also_empty: Vec<i32> = my_vec!(make an empty vec);
+    println!("{also_empty:#?}");
+
+    let single_number = my_vec![1];
+    println!("{single_number:#?}");
+
+    let three_numbers = my_vec![1, 2, 3];
+    println!("{three_numbers:#?}");
 }
